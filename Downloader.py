54e18982b72dc876e9654b641146b212f3f491b9
Martin Bies <MBies87@googlemail.com>
2020-01-11 20:39:52 +0000

    Add Downloader.py

---
+++ Downloader.py
@@ -0,0 +1,30 @@
+# how to download stock data for free in 2019
+
+import pandas as pd
+import pandas_datareader.data as web
+import datetime as dt
+from datetime import datetime
+import os
+
+def get_stock_data():
+    tickers = ['TSLA', 'MCD', 'AAPL', 'GOOGL', 'XOM' ]
+    #tickers = ['MCD', 'AAPL', 'GOOGL', 'XOM' ] # capitalize tickers
+    
+    start = dt.datetime(2015,3,5) # can import 5 years max with iex
+    end = dt.datetime.today()
+    
+    if not os.path.exists('stockdata'):
+        os.makedirs('stockdata')
+        
+    for ticker in tickers:
+        print (ticker)
+        try:
+            df = web.DataReader(ticker, 'yahoo', start, end )
+            print(df.head())
+            df.to_csv('stockdata/'+ticker+'.csv')
+            print(ticker,'downloaded')
+            print('\n')
+        except Exception as e:
+            print( e, 'error' )
+
+get_stock_data()
