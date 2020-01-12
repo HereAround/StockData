3e770dae73c71bd8bcae33fd45af39bb246e7474
Martin Bies <MBies87@googlemail.com>
2020-01-12 16:38:18 +0000

    Remove the virtual environment from the repository Updated Readme accordingly

--- Main.py
+++ Main.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3.7
 
-# the above ensures that this code is being executed with python3.7 if we simply issue this script
 
-# (1) Create a virtual environment in which we run python3.7
-# (1) This is to protect the system phyton.
-# (1) To this end, it will create a directory "virt-python3.7" in which we run a fresh copy of python3.7
-# (1) In particular, all packages have to be installed in this virtual environment (otherwise: module not found)
-python3.7 -m venv virt-python3.7
-
-# (2) Activate the virtual environment
-source virt-python3.7/bin/activate
-
-# (3) Load all necessary libraries
+# (1) Load all necessary libraries
 import pandas as pd
 import pandas_datareader.data as web
 import datetime as dt
@@ -24,10 +14,11 @@ import numpy as np
 from scipy.stats.stats import pearsonr 
 #style.use('ggplot')
 
-# load our functions
+
+# (2) Load our functions
 #import Downloader.py
 #import trader_001.py
 
-# execute our functions
 
+# (3) Execute our functions
 #get_stock_data()
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # StockData
-Download stock data...
+Analyse stock data with python3
 
-with python3
+
+For best practice with phyton3, create a virtual environment and run python 3 in this environment. This is to protect the python used by the system.
+
+In ubuntu 18.04, the following command creates a virtual environment "virt-python3.7". This is a folder located in the directory where this command is being issued:
+
+python3.7 -m venv virt-python3.7
+
+Subsequently, activate this virtual environment by issuing
+
+source virt-python3.7/bin/activate
+
