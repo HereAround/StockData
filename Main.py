b36a1e53c39211aa1d47c8d705e35ace987575f1
Martin Bies <MBies87@googlemail.com>
2020-01-18 15:57:34 +0000

    Use new functionality for loading data in Main.py

--- Main.py
+++ Main.py
@@ -7,38 +7,22 @@ from datetime import datetime
 
 # (1) Load functionality
 from Downloader import save_stock_data, get_stock_data
+from Downloader import get_and_prepare_stock_data
 from Trader import trade_rolling_average, optimize_rolling_length
 
 
-# (2) Pick title
+# (2) Set constants
 source = 'yahoo'
-ticker = 'GOOGL'
-ticker = 'AMZN'
-ticker = 'MSFT'
-ticker = 'AAPL'
-ticker = 'SAP'
-ticker = '^DJI'
-ticker = 'CON.DE'
-ticker = 'TTR1.DE'
-ticker = 'DRI.DE'
-ticker = 'ZO1.DE'
-ticker = '005930.KS' # Samsung?
-ticker = 'LEO'
-# Next: Automatically find the ticker for a given WKN or ISDN
-
-# (3) Pick start and end date
+tickers = [ 'GOOGL', 'AMZN', 'MSFT', 'AAPL', 'SAP', '^DJI', 'CON.DE', 'TTR1.DE', 'DRI.DE', 'ZO1.DE', '005930.KS', 'LEO' ]
 start = dt.datetime(1980,3,5)
 end = dt.datetime.today()
 
 
-# (4) Pick how much to invest in single order and how much one order costs in fees
-amount = 10000
-fee = 5
+# (3) Obtain stock data
+data = get_and_prepare_stock_data( source, tickers, start, end )
 
 
-# (5) Extract stock data
-data = get_stock_data( source, ticker, start, end )
-
-
-# (6) Find ideal rolling length
-max_value = optimize_rolling_length( data, amount, fee )
+# (5) Pick how much to invest in single order and how much one order costs in fees
+#amount = 10000
+#fee = 5
+#max_value = optimize_rolling_length( data, amount, fee )
