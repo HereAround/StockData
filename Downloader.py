e4023fcb0308e02d78c57561e7d110a2287c6c44
Martin Bies <MBies87@googlemail.com>
2020-01-18 15:47:59 +0000

    Implement improved download function

--- Downloader.py
+++ Downloader.py
@@ -22,19 +22,34 @@ def save_stock_data( source, tickers, start, end ):
 
 
 # (2) Get stock_data
-def get_stock_data( source, ticker, start, end ):
+def get_stock_data( source, tickers, start_date, end_date ):
     
-    print (ticker)
+    # (1) download data of the tickers
+    print( 'Download started' )
+    print( 'Tickers of interest: ' + str( tickers ) )
     try:
-        df = web.DataReader(ticker, source, start, end )
-        print(ticker,'downloaded')
-        print('\n')
+        data = web.DataReader( tickers, 'yahoo', start_date, end_date )
+        print( 'Download finished \n' )
     except Exception as e:
         print( e, 'error' )
     
-    # use date as index in this dataframe
-    df.reset_index(inplace=True)
-    df.set_index("Date", inplace=True)
+    # (2) prepare the data
+    
+    # 2.1: We work with the opening values
+    # 2.1: We work with the opening values
+    data_selected = data['Open']
+
+    # 2.2 Index the data by all working days between start and end
+    # 2.2 Whenever the corresponding data is not present, we will in the missing value.
+    # 2.2 For this we use the latest available price
+    all_weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
+    data_selected = data_selected.reindex(all_weekdays)
+    data_selected = data_selected.fillna(method='ffill')
+
+    # 2.3 Extract data for each ticker
+    data_selected_split = []
+    for ticker in tickers:
+        data_selected_split.append( data_selected.loc[:, ticker] )
     
     # now return the data
-    return df
+    return data_selected_split
