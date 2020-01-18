4a76035d6ac1a46cc036319497c829e02098f9e6
Martin Bies <MBies87@googlemail.com>
2020-01-18 18:15:33 +0000

    Improve trader

--- Trader.py
+++ Trader.py
@@ -1,89 +1,113 @@
 # (1) Load necessary libraries
-import pandas as pd
-import pandas_datareader.data as web
-import datetime as dt
-from datetime import datetime
-import os
 import matplotlib.pyplot as plt
 from matplotlib import style
 import math
 import numpy as np
-from scipy.stats.stats import pearsonr 
-style.use('ggplot')
+from scipy.stats.stats import pearsonr
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 
 
 # (2) Trade by rolling average
 def trade_rolling_average( data, roll_length, amount, fee, display ):
 
-    # Compute rolling average
-    S = data['Open'].dropna()
-    length = len(S)
-    rolling_mean = S.rolling(window=roll_length,min_periods=0).mean()
-    data['Rolling-Mean'] = rolling_mean
-
-    # Compute the differences
-    data['Difference'] = data['Open'] - data['Rolling-Mean']
-
-    # Trade:
+    # 2.1 prepare data for trading
+    rolling_mean = data.rolling(window=roll_length,min_periods=0).mean()
+    difference = data - rolling_mean
+    
+    # 2.2 set constant
+    cash = amount
+    shares = 0
+    invested = False
+    bancrupt = False
+    trades = 0
+    value = [ ]
+    max_share_price = data.max()
+    
+    # 2.3 Trade:
     # (1) Wait for good moment for investment
     # (2) Wail for good instance to sell
     # -> Repeat
-    money = amount
-    shares = 0
-    invested = False
-    trades = 0
     if display:
         print( 'Investor starts...' )
 
-    for i in range(2, length ):
+    for i in range( 0, len( data ) ):
         
-        # check if we should act
-        if np.sign( data['Difference'][i-2] ) != np.sign( data['Difference'][i] ):
-
-            if data['Difference'][i] > 0 and invested == False:
-                # -> Buy!
-                shares = ( money - fee ) / data['Open'][ i ]
-                money = 0
-                invested = True
-                trades = trades + 1
+        # Wait at least 10 days
+        # But in any case long enough to have finite values for the differences (can identify tendency)
+        if i > 10 and not math.isnan( difference[ i - 2 ] ) and not math.isnan( difference[ i ] ) and not bancrupt:
             
-            if data['Difference'][i] < 0 and invested == True:
-                # -> Sell!
-                money = shares * data['Open'][ i ] - fee
-                shares = 0
-                invested = False
-                trades = trades + 1
+            # check if we should act
+            if np.sign( difference[i-2] ) != np.sign( difference[i] ):
+
+                if difference[i] > 0 and invested == False and cash > fee:
+                    # -> Buy!
+                    shares = ( cash - fee ) / data[ i ]
+                    cash = 0
+                    invested = True
+                    trades = trades + 1
+                    if display:
+                        print( "Buy:" )
+                        print( "Value: " + str( data[ i ] ) )
+                        print( "Difference: " + str( difference[i ] ) )
+                        print( "Tendency: " + str( difference[ i ] - difference[ i - 2 ] ) )
+                        print( "(Money,Shares) = [" + str( cash ) + ", " + str( shares ) + " ]" )
+                        print( "Total value: " + str( cash + shares * data[ i ] ) + " \n" )
+            
+                if difference[i] < 0 and invested == True and shares * data[ i ] - fee > 0:
+                    # -> Sell!
+                    cash = shares * data[ i ] - fee
+                    shares = 0
+                    invested = False
+                    trades = trades + 1
+                    if display:
+                        print( "Sell:" )
+                        print( "Value: " + str( data[ i ] ) )
+                        print( "Difference: " + str( difference[i ] ) )
+                        print( "Tendency: " + str( difference[ i ] - difference[ i - 2 ] ) )
+                        print( "(Money,Shares) = [" + str( cash ) + ", " + str( shares ) + " ]" )
+                        print( "Total value: " + str( cash + shares * data[ i ] ) + " \n" )
+        
+        # check if the trader is very bad and bancrupt
+        if cash + shares * data[ i ] < 0:
+            cash = 0
+            shares = 0
+            bancrupt = True
+            print( "Trader bancrupt" )
+
+        value.append( ( cash + shares * data[ i ] ) / amount * max_share_price )
     
     if display:
-        print( 'Investor ends' )
-        
-        print( '\n' )
+        print( 'Investor ends \n' )
         print( 'Original investment: ' + str( amount ) )
         print( 'Length of rolling average: ' + str( roll_length ) + ' days' )
         print( 'Total trades: ', str( trades ) )
         print( 'Total fees: ', str( trades * fee ) )
         print( '\n' )
         print( 'Shares: ', str( shares ) )
-        print( 'Money: ', str( money ) )
-        print( 'Total value: ', str( data['Open'][ length - 1 ] * shares + money ) )
-        print( 'Increase of value: ' + str( 100 * ( data['Open'][ length - 1 ] * shares + money ) / amount - 100 ) + '%' )
+        print( 'Money: ', str( cash ) )
+        print( 'Total value: ', str( data[ len( data ) - 1 ] * shares + cash ) )
+        print( 'Increase of value: ' + str( 100 * ( data[ len( data ) - 1 ] * shares + cash ) / amount - 100 ) + '%' )
         print( '\n' )
-        print( 'Final difference: ' + str( data['Difference'][ length - 1 ] ) )
-        if data['Difference'][ length - 1 ] > 0:
+        print( 'Final difference: ' + str( difference[ len( data ) - 1 ] ) )
+        if difference[ len( data ) - 1 ] > 0:
             print( 'Tendency: Value increases -> Hold/Buy' )
-        if data['Difference'][ length - 1 ] <= 0:
+        if difference[ len( data ) - 1 ] <= 0:
             print( 'Tendency: Value decreases -> Sell' )
-        data['Pos'] = range(1, len(data) + 1)
-        data.plot(x='Pos', y=['Open', 'Difference', 'Rolling-Mean' ], kind="line")
-        plt.legend()
+        pos = list(data.index.values)
+        plt.plot( pos, data, label = 'opening value' )
+        plt.plot( pos, rolling_mean, label = 'rolling mean' )
+        plt.plot( pos, difference, label = 'difference' )
+        plt.plot( pos, value, label = 'total trading value' )
         plt.title("Opening prices, rolling average and difference")
         plt.ylabel('Value', fontsize = 14)
         plt.xlabel('Trading days', fontsize = 14)
         plt.grid(which = "major", color = 'k', linestyle = '-.', linewidth = 0.5 )
+        plt.legend()
         plt.show()
     
     # return total value
-    return data['Open'][ length - 1 ] * shares + money
+    return data[ len( data ) - 1 ] * shares + cash
 
 
 # (3) Optimize rolling length
