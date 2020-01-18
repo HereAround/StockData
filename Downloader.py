164dc1878fc32b160f9310c102fe74273da93114
Martin Bies <MBies87@googlemail.com>
2020-01-18 15:06:00 +0000

    Remove unneccessary imports

--- Downloader.py
+++ Downloader.py
@@ -1,15 +1,7 @@
 # (1) Load necessary libraries
 import pandas as pd
 import pandas_datareader.data as web
-import datetime as dt
-from datetime import datetime
 import os
-import matplotlib.pyplot as plt
-from matplotlib import style
-import math
-import numpy as np
-from scipy.stats.stats import pearsonr 
-style.use('ggplot')
 
 
 # (2) Get stock_data
